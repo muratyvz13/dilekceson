fa312d7d391457928a74acd535b1f01dab751b1a
Murat Yavuz <muratyavuz@192.168.1.71>
2022-06-22 05:32:27 +0300

    Updated contact informations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-//import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-     
-
-    </div>
-  );
-}
-
-export default App;
+import React, { useRef } from "react";
+  import emailjs, { init } from "@emailjs/browser";
+  function App() {
+    const handleSubmit = (e) => {
+      e.preventDefault();
+    };
+    return (
+      <div className="container">
+        
+        
+        
+      </div>
+    );
+  }
+  export default App;
